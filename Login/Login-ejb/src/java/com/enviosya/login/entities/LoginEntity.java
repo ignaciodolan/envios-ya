ce0f64dab03784f1312a8df1f93b7b96cb0507c4
Mauro Caruso <carusoferrari@gmail.com>
2017-06-26 18:10:10 -0300

    Se le pega a la api de facebook para el login, se crean dto,dao, entity, etc

---
+++ Login/Login-ejb/src/java/com/enviosya/login/entities/LoginEntity.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.enviosya.login.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Ruso
+ */
+@Entity
+public class LoginEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String _user;
+    
+    private String token;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date startDate;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date endDate;
+
+    public LoginEntity() {
+    }
+
+    public LoginEntity(String user, String token, Date startDate, Date endDate) {
+        this._user = user;
+        this.token = token;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof LoginEntity)) {
+            return false;
+        }
+        LoginEntity other = (LoginEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.enviosya.login.entities.LoginEntity[ id=" + id + " ]";
+    }
+    
+}
---
+++ Login/Login-war/src/java/com/enviosya/login/resources/LoginResource.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.enviosya.login.resources;
+
+import com.enviosya.logger.LoggerEnviosYa;
+import com.enviosya.login.beans.LoginBean;
+import com.enviosya.login.dto.LoginDTO;
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author Ruso
+ */
+@Path("login")
+public class LoginResource {
+
+    private final LoggerEnviosYa logger = new LoggerEnviosYa(LoginResource.class);
+
+    @EJB
+    private LoginBean loginBean;
+
+    private final Gson gson = new Gson();
+
+    public LoginResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.enviosya.login.resources.LoginResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Path("/{id}/verifyToken/{token}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response verifyToken(@PathParam("id") Long id, @PathParam("token") String token) {
+        StringBuilder message;
+        Response response;
+        try {
+            loginBean.verifyToken(token, id);
+            message = new StringBuilder();
+            message.append("Token verification:");
+            message.append(gson.toJson(token));
+            logger.success(message.toString());
+            response = Response.ok().entity(gson.toJson("Eltoken")).build();
+        } catch (Exception e) {
+            message = new StringBuilder();
+            message.append("[Exception]: ");
+            message.append(e.getMessage());
+            logger.error(message.toString());
+            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(message.toString()).build();
+        }
+        return response;
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response doLogin(String messageJson) {
+        StringBuilder message;
+        Response response;
+        LoginDTO loginDTO = gson.fromJson(messageJson, LoginDTO.class);
+        try {
+            
+            //Llamar a la pagina de login
+            //Recibir la data y mandarsela al bean
+            response = Response.ok().entity(gson.toJson("Login success")).build();
+        } catch (JsonSyntaxException e) {
+            message = new StringBuilder();
+            message.append("[Message Syntax error gson]");
+            message.append(e.getMessage());
+            logger.error(message.toString());
+            response = Response.status(Response.Status.BAD_REQUEST).entity(message.toString()).build();
+        } catch (JsonIOException e) {
+            message = new StringBuilder();
+            message.append("[Message IO error gson]");
+            message.append(e.getMessage());
+            logger.error(message.toString());
+            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(message.toString()).build();
+        }
+        return response;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response register(String messageJson) {
+        StringBuilder message;
+        Response response;
+        try {
+            //Agarrar los datos de facebook y del json xa realizar el registro.
+           //loginBean.register();
+           response = Response.ok().entity(gson.toJson("Register success")).build();
+        }
+       catch (JsonSyntaxException e) {
+            message = new StringBuilder();
+            message.append("[Message Syntax error gson]");
+            message.append(e.getMessage());
+            logger.error(message.toString());
+            response = Response.status(Response.Status.BAD_REQUEST).entity(message.toString()).build();
+        } catch (JsonIOException e) {
+            message = new StringBuilder();
+            message.append("[Message IO error gson]");
+            message.append(e.getMessage());
+            logger.error(message.toString());
+            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(message.toString()).build();
+        }
+        return response;
+    }
+}
